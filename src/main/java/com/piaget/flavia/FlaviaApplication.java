57a79d14ac722ead3711636c31ff8820fb7e84dd
flavia mariana <flaviamarianapiaget@gmail.com>
2025-05-23 11:04:57 -0300

    barbi

---
+++ src/main/java/com/piaget/flavia/FlaviaApplication.java
@@ -0,0 +1,13 @@
+package com.piaget.flavia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlaviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlaviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piaget/flavia/FlaviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piaget.flavia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlaviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
